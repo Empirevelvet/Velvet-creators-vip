9512d96af17ab5731daa5893b2cdee94a1aac2aa
Empirevelvet <Empirevelvet@proton.me>
2025-08-16 14:45:13 +0200

    Update create-order.js

--- netlify/functions/create-order.js
+++ netlify/functions/create-order.js
@@ -1,13 +1,33 @@
+const fetch = require("node-fetch");
+
 exports.handler = async (event) => {
   try {
     const body = JSON.parse(event.body);
 
-    const order = {
-      intent: "CAPTURE",
-      purchase_units: [{
-        amount: { currency_code: "CHF", value: body.amount }
-      }]
-    };
+    // Authentification PayPal
+    const auth = Buffer.from(
+      process.env.PAYPAL_CREATRICES_ID + ":" + process.env.PAYPAL_VIP_SECRET
+    ).toString("base64");
+
+    // Créer une commande PayPal
+    const response = await fetch("https://api-m.paypal.com/v2/checkout/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Basic ${auth}`
+      },
+      body: JSON.stringify({
+        intent: "CAPTURE",
+        purchase_units: [{
+          amount: {
+            currency_code: "CHF",
+            value: body.amount
+          }
+        }]
+      })
+    });
+
+    const order = await response.json();
 
     return {
       statusCode: 200,
