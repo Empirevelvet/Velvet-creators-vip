1d0b6c170cc0b1de0db017b69db5f85d78b59a91
Empirevelvet <Empirevelvet@proton.me>
2025-08-18 21:17:24 +0200

    Create ping.js

---
+++ netlify/functions/ping.js
@@ -0,0 +1,8 @@
+// CommonJS (aucune dépendance)
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ ok: true, time: new Date().toISOString() })
+  };
+};
